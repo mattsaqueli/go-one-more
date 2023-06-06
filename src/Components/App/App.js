03466337697a44b70d4cadf7ba5e6e2cbb192680
Matt Saqueli <msaqueli22@turing.edu>
2023-06-06 10:44:21 -0500

    feat: Add Route, setup App and Header components

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Route } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> go one more </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
