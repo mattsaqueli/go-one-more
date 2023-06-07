9ccbc78d44945d291abb042fdad81856ea83e21a
Matt Saqueli <msaqueli22@turing.edu>
2023-06-07 12:23:56 -0500

    Adjusts Favorites page styling and display

--- src/Components/Favorites/Favorites.js
+++ src/Components/Favorites/Favorites.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Favorites.css'
 
 const Favorites = ({ favorites }) => {
   const favoriteQuotes = favorites.map((favorite, index) => (
     <p key={index}>{favorite}</p>
   ));
 
-  return <div className='favorites-container'>{favoriteQuotes}</div>;
+  return (
+  <div className='favorites-container'>{favoriteQuotes}</div>
+  )
 };
 
 export default Favorites;
