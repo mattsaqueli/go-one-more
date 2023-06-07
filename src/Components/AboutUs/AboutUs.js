90e0f5260245d0efc6382e36ee29dd23bce200e3
Matt Saqueli <msaqueli22@turing.edu>
2023-06-07 13:02:42 -0500

    Add About Us section, update CSS background

---
+++ src/Components/AboutUs/AboutUs.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './AboutUs.css'
+import { Link } from 'react-router-dom'
+
+const AboutUs = () => {
+
+  return (
+    <article className='about-container'>
+      <h1>About Us</h1>
+      <p>
+        
+Welcome to Go One More, the platform dedicated to supporting veterans in their transition from the military and addressing mental health challenges. We understand that transitioning to civilian life can be a daunting process, and we are here to provide a helping hand and a source of inspiration to improve your mental well-being on this transformative journey.
+
+Go One More was created by a veteran who has personally experienced the unique struggles faced by those transitioning out of the military. We have designed our platform to specifically address the needs of veterans and provide them with the support they deserve. Our founder's firsthand experience allows us to offer valuable insights and a deep understanding of the challenges you may encounter during this phase of your life.
+
+Through our app, we provide a daily dose of stoic quotes that are carefully selected to uplift and empower you. Stoicism, an ancient philosophy, has proven to be a powerful tool for developing resilience, embracing change, and finding inner strength. These quotes are meant to inspire self-reflection, promote emotional well-being, and provide a sense of solace and guidance as you navigate the complexities of life beyond the military.
+
+Go One More believes in the power of going the extra mile, pushing beyond perceived limits, and embracing the idea that there is always more to give. Our app aims to help you cultivate a mindset of mental fortitude, equipping you with the tools necessary to face challenges head-on and thrive in your post-military life. Together, we can redefine what it means to transition successfully and find fulfillment in the next chapter of your journey.
+
+
+      </p>
+    </article>
+  )
+}
+
+export default AboutUs
